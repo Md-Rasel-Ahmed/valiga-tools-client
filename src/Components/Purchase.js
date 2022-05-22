e9dfe68b5bfc74eb5ff0be54ba8514b0c8c6d7d7
Rasel-ahemd <nhd305812@gmail.com>
2022-05-22 16:38:07 +0500

    fireabse setup done

--- src/Components/Purchase.js
+++ src/Components/Purchase.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 const Purchase = () => {
   const { id } = useParams();
+  const [item, setItem] = useState(null);
+  useEffect(() => {
+    fetch("services.json0")
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }, []);
   return (
-    <div>
-      <h2>Purchase{id}</h2>
+    <div className="grid lg:grid-cols-2  px-5">
+      <div>items</div>
+      <div>Details</div>
     </div>
   );
 };
--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,15 +6,19 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCw9S27Fmgwv4SgkOhp3-decAQ5ZL4hhgg",
-  authDomain: "final-project-a7f75.firebaseapp.com",
-  projectId: "final-project-a7f75",
-  storageBucket: "final-project-a7f75.appspot.com",
-  messagingSenderId: "273980413528",
-  appId: "1:273980413528:web:1b2a2ee79a5d6a06badc2d",
+  apiKey: "AIzaSyDHTfYI4FSRVhzFHgExCD6v4aRJ7Tm5vbA",
+  authDomain: "valiga-hardware.firebaseapp.com",
+  projectId: "valiga-hardware",
+  storageBucket: "valiga-hardware.appspot.com",
+  messagingSenderId: "1060092597434",
+  appId: "1:1060092597434:web:727c1f01e3909d0db49e83",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 export default auth;
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
